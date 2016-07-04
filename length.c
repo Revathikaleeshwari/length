df7878d2eb4ef58e7b0cb556ecb91ec57f53461b
Revathi N <nrevathibe38@gmail.com>
2016-07-04 11:45:18 +0530

    Create length.c

---
+++ length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50];
+int length;
+printf("Enter the character\n");
+scanf(a);
+length=strlen(a);
+printf("\nNumber of the charactor is=%d",length);
+return 0;
+}
